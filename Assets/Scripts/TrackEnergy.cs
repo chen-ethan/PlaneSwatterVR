e5bd57dfd15e7b434ff6075e5ca20979053f3580
Aditya Choksi <adchoksi@uci.edu>
2018-12-03 13:56:30 -0800

    energy up and down with space

---
+++ Assets/Scripts/TrackEnergy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackEnergy : MonoBehaviour {
+	public float maxSize;
+	public float growFactor;
+	private float minSize;
+	private float timer;
+	private bool flip;
+
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+		minSize = 0.2f;
+		
+	}
+	
+
+	void Update () {
+		if (!Input.GetKey("space") && transform.localScale.y > minSize){
+			transform.localScale -= new Vector3(0,growFactor,0);
+		}
+		else{
+			if(transform.localScale.y < maxSize){
+				transform.localScale += new Vector3(0,growFactor,0);
+			}
+		}	
+	}
+}
