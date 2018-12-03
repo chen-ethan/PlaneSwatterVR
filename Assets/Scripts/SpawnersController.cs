6310f4044bbbe2477cdaadf6ce8c4333fbcf55ba
chen-ethan <ethanchen111@gmail.com>
2018-12-03 14:07:30 -0800

    added spawnerChild scripting

---
+++ Assets/Scripts/SpawnersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnersController : MonoBehaviour {
+
+	private static float spawnerTimer;
+	//public int numberOfSpawns;
+	public float delay;
+	void Start () {
+		spawnerTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnerTimer += Time.deltaTime;
+		int children = transform.childCount;
+		if(spawnerTimer >= delay){
+			int plane = (int)Random.Range(0,children);
+			Debug.Log("plane #" + plane);
+			spawnerTimer = 0.0f;
+		}
+	}
+}
