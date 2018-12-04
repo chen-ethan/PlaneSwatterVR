66304f3f2dee6fb61cfcc15b897f231ad91d0a4f
Aditya Choksi <adchoksi@uci.edu>
2018-12-03 16:10:04 -0800

    the arm is yolked

--- Assets/Scripts/TrackEnergy.cs
+++ Assets/Scripts/TrackEnergy.cs
@@ -3,28 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackEnergy : MonoBehaviour {
+	public bool rage;
 	public float maxSize;
 	public float growFactor;
+	public float lockTime;
 	private float minSize;
-	private float timer;
+	private float lockTimer;
 	private bool flip;
+	private bool lockdown;
 
 
 	// Use this for initialization
 	void Start () {
-		timer = 0.0f;
-		minSize = 0.2f;
+		minSize = 0.0f;
+		rage = false;
+		lockdown = false;
 		
 	}
 	
 
 	void Update () {
-		if (!Input.GetKey("space") && transform.localScale.y > minSize){
+		if(lockdown){
+			lockTimer -= Time.deltaTime;
+			if(lockTimer < 0){
+				lockdown = false;
+			}
+		}
+
+		if ((!Input.GetKey("space") || lockdown) && transform.localScale.y > minSize){
 			transform.localScale -= new Vector3(0,growFactor,0);
+			rage = false;
 		}
 		else{
-			if(transform.localScale.y < maxSize){
+			if(transform.localScale.y <= minSize){
+				transform.localScale = new Vector3(transform.localScale.x,0,transform.localScale.z);
+				if(!lockdown){
+					rage = false;
+				}
+			}
+			if((transform.localScale.y <= maxSize) && Input.GetKey("space")){
 				transform.localScale += new Vector3(0,growFactor,0);
+				if(!lockdown){
+					rage = true;
+				}
+			}
+			if(transform.localScale.y >= maxSize){
+				Debug.Log("max reached");
+				lockdown = true;
+				lockTimer = lockTime;
+				rage = false;
 			}
 		}	
 	}
